6d2a7b1e373c4bc7028a838e105c0ae0d0576bce
Agus <agus@agus.com>
2016-05-10 02:42:14 -0300

    carpeta

---
+++ data copy.js
@@ -0,0 +1,57 @@
+var layout_data = [
+    { "len": 31, "color": "#8dd3c7", "label": "January", "id": "january" },
+    { "len": 28, "color": "#ffffb3", "label": "February", "id": "february" },
+    { "len": 31, "color": "#bebada", "label": "March", "id": "march" },
+    { "len": 30, "color": "#fb8072", "label": "April", "id": "april" },
+    { "len": 31, "color": "#80b1d3", "label": "May", "id": "may" },
+    { "len": 30, "color": "#fdb462", "label": "June", "id": "june" },
+    { "len": 31, "color": "#b3de69", "label": "July", "id": "july" },
+    { "len": 31, "color": "#fccde5", "label": "August", "id": "august" },
+    { "len": 30, "color": "#d9d9d9", "label": "September", "id": "september" },
+    { "len": 31, "color": "#bc80bd", "label": "October", "id": "october" },
+    { "len": 30, "color": "#ccebc5", "label": "November", "id": "november" },
+    { "len": 31, "color": "#ffed6f", "label": "December", "id": "december" }
+];
+var heatmap = [
+    ['january',1,2,3],
+    ['january',2,3,145],
+    ['january',2,23,1543],
+    ['january',2,21,13],
+    ['january',12,1,541],
+    ['january',21,2,321],
+    ['january',32,3,761],
+    ['january',23,2,1],
+    ['january',12,2,3],
+    ['january',26,3,145],
+    ['january',2132,23,1543],
+    ['january',22,21,13],
+    ['january',12,1,541],
+    ['january',21,2,321],
+    ['january',32,3,761],
+    ['january',23,2,1],
+    ['february',228,3,1],
+    ['april',286,27,1684766],
+    ['may',286,1,1577419],
+    ['june',28232326,2,15311],
+    ['july',289,3,88266], 
+    ['august',211,1,1169570],
+    ['september',263,111,1163321],
+    ['october',236,114,1163321],
+    ['november',186,12,1163321],
+    ['december',302,1,1163321]
+];
+
+days_off = [
+     ['january',1,2,1],
+    ['january',2,3,2],
+    ['february',3,4,3],
+    ['april',286,27,1684766],
+    ['may',286,1,1577419],
+    ['june',286,2,15311]
+    // ['july',289,3,88266], 
+    // ['august',211,1,1169570],
+    // ['september',263,111,1163321],
+    // ['october',236,114,1163321],
+    // ['november',186,12,1163321],
+    // ['december',302,1,1163321]
+];
